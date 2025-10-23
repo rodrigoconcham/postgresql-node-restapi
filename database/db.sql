de566ba713006c4e1c232f5153d4e05c8242d200
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-10-23 17:31:09 -0300

    database

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email)
+    VALUES ('John Doe', 'john@gmail.com'),
+           ('Jane Doe', 'jane@gmail.com');
+
+
+select * from users;               
